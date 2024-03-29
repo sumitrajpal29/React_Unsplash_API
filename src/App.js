2c68a8e892a549ea104cc2059200d0caa3f9d92d
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2024-03-30 00:54:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [image, setImage] = useState('');
+  useEffect(() => {
+    fetchRandomImage();
+  }, []);
+
+  const fetchRandomImage = () => {
+    fetch(`https://api.unsplash.com/photos/random?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&orientation=landscape`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.urls && data.urls.regular) {
+          setImage(data.urls.regular);
+          console.log("workingggg");
+        }
+        else {
+          console.log('Invalid response from unsplash API:', data);
+        }
+      })
+      .catch(err => {
+        console.log('Error fetching random image:', err);
+      });
+  };
+
+  const Time = Date();
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${image})` }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={image} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {Time}
         </p>
         <a
           className="App-link"
---
+++ src/getImage.js
@@ -0,0 +1,5 @@
+export default function getImage() {
+    return <>
+        <h1>Got Image</h1>
+    </>
+}
